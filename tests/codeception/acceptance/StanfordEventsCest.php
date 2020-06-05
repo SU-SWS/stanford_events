f161174b52d1cc80c5e1559715c4d08a7e42e9e0
Shea McKinney <sherakama@gmail.com>
2020-06-05 09:58:44 -0700

    Tests stub.

---
+++ tests/codeception/acceptance/StanfordEventsCest.php
@@ -0,0 +1,29 @@
+<?php
+
+class StanfordEventsCest {
+
+  /**
+   * Events
+   */
+  public function testEventNodeForm(AcceptanceTester $I) {
+    $I->logInWithRole('administrator');
+    $I->amOnPage('/node/add/stanford_event');
+    $I->canSee('Event Title');
+    $I->canSee('Event Type');
+    $I->canSee('Subheadline');
+    $I->canSee('Dek');
+    $I->canSee('SPONSOR');
+    $I->canSee('Body');
+    $I->canSee('Components');
+    $I->canSee('DATE & TIME');
+    $I->canSee('External Source');
+    $I->canSee('LOCATION');
+    $I->canSee('MAP LINK');
+    $I->canSee('Contact Email');
+    $I->canSee('Contact Telephone');
+    $I->canSee('Audience');
+    $I->canSee('CALL TO ACTION BUTTON');
+    $I->canSee('Schedule');
+  }
+
+}
