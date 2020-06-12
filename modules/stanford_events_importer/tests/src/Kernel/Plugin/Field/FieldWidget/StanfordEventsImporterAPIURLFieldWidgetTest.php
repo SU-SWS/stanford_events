3f861c46fe17254ae735879bf3d67b0547b9a74a
Sherakama <sherakama@gmail.com>
2020-06-11 20:28:49 -0700

    D8ORE-1623: Stanford Events, Importer, and Event Series (#2)
    D8CORE-1623: Stanford Events Accelerant.

---
+++ modules/stanford_events_importer/tests/src/Kernel/Plugin/Field/FieldWidget/StanfordEventsImporterAPIURLFieldWidgetTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\Tests\stanford_events_importer\Kernel\Plugin\Field\FieldWidget;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Core\Form\FormState;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+
+/**
+ * Class StanfordEventsImporterAPIURLFieldWidgetTest.php
+ *
+ * @group stanford_events_importer
+ * @coversDefaultClass \Drupal\stanford_events_importer\Plugin\Field\FieldWidget\StanfordEventsImporterAPIURLFieldWidget
+ */
+class StanfordEventsImporterAPIURLFieldWidgetTest extends KernelTestBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  protected static $modules = [
+    'system',
+    'path_alias',
+    'node',
+    'user',
+    'link',
+    'datetime',
+    'field',
+    'stanford_events_importer'
+  ];
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('node');
+    $this->installEntitySchema('field_config');
+
+    NodeType::create(['type' => 'page'])->save();
+
+    $field_storage = FieldStorageConfig::create([
+      'field_name' => 'field_xml_link',
+      'entity_type' => 'node',
+      'type' => 'link',
+      'cardinality' => -1,
+    ]);
+    $field_storage->save();
+
+    $field = FieldConfig::create([
+      'field_name' => 'field_xml_link',
+      'entity_type' => 'node',
+      'bundle' => 'page',
+    ]);
+    $field->save();
+  }
+
+  /**
+   * Test the entity form is displayed correctly.
+   */
+  public function testWidgetForm() {
+    $node = Node::create([
+      'type' => 'page',
+    ]);
+    /** @var \Drupal\Core\Entity\Display\EntityFormDisplayInterface $entity_form_display */
+    $entity_form_display = EntityFormDisplay::create([
+      'targetEntityType' => 'node',
+      'bundle' => 'page',
+      'mode' => 'default',
+      'status' => TRUE,
+    ]);
+    $entity_form_display->setComponent('field_xml_link', ['type' => 'stanford_events_importer_apiurl_field_widget'])
+      ->removeComponent('created')
+      ->save();
+
+    $form = [];
+    $form_state = new FormState();
+
+    $entity_form_display->buildForm($node, $form, $form_state);
+
+    $widget_value = $form['field_xml_link']['widget'][0];
+    $this->assertIsArray($widget_value);
+  }
+
+}
---
+++ modules/stanford_events_importer/tests/src/Unit/StanfordEventsImporterTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Drupal\Tests\stanford_events_importer\Unit\Plugin\Field\FieldWidget;
+
+use Drupal\stanford_events_importer\StanfordEventsImporter;
+use Drupal\Tests\UnitTestCase;
+use GuzzleHttp\Client;
+use Drupal\Core\Cache\DatabaseBackend;
+
+/**
+ * Class StanfordEventsImporterTest
+ *
+ * @group stanford_events_importer
+ * @coversDefaultClass \Drupal\stanford_events_importer\StanfordEventsImporter
+ */
+class StanfordEventsImporterTest extends UnitTestCase {
+
+  /**
+   * @var \Drupal\stanford_events_importer\StanfordEventsImporter
+   */
+  public $plugin;
+
+  /**
+   * @var \GuzzleHttp\ClientInterface
+   */
+  public $client;
+
+  /**
+   * @var string
+   */
+  public $xml;
+
+  public function setup() {
+    $this->client = new Client();
+    $this->plugin = new StanfordEventsImporter($this->client);
+  }
+
+  /**
+   * Test Fetch.
+   */
+  public function testFetchXML() {
+    $this->xml = $this->plugin->fetchXML();
+    $this->assertContains("Arts", $this->xml);
+
+    $orgs = $this->plugin->fetchXML('organization-list');
+    $this->assertContains("Bing Overseas Studies Program", $orgs);
+  }
+
+  /**
+   * Test Parse.
+   */
+  public function testParseXML() {
+
+    $xml_string = <<<EOD
+<CategoryList>
+<Category>
+<guid>0</guid>
+<categoryID>0</categoryID>
+<name>Arts</name>
+<type>3</type>
+<description>Arts</description>
+<tag>arts</tag>
+</Category>
+<Category>
+<guid>28</guid>
+<categoryID>28</categoryID>
+<name>Careers</name>
+<type>2</type>
+<description>Career Development</description>
+<tag>careers</tag>
+</Category>
+<Category>
+<guid>19</guid>
+<categoryID>19</categoryID>
+<name>Class</name>
+<type>1</type>
+<description>Classes</description>
+<tag>class</tag>
+</Category>
+<Category>
+<guid>3</guid>
+<categoryID>3</categoryID>
+<name>Conference / Symposium</name>
+<type>1</type>
+<description>Conferences / Symposia</description>
+<tag>conference</tag>
+</Category>
+<Category>
+<guid>5</guid>
+<categoryID>5</categoryID>
+<name>Dance</name>
+<type>3</type>
+<description>Dance</description>
+<tag>dance</tag>
+</Category>
+</CategoryList>
+EOD;
+
+    $args = [
+      'guids' => '/CategoryList/Category/guid',
+      'label' => '/CategoryList/Category/name',
+    ];
+    $result = $this->plugin->parseXML($xml_string, $args);
+    $this->assertIsArray($result);
+    $this->assertEquals("Class", $result[19]);
+  }
+
+}
+
