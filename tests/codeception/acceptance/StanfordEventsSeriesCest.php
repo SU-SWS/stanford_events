ee8408463818d0728bb0419ba06fcd4d9e0788b9
Mike Decker <pookmish@gmail.com>
2020-12-10 15:18:37 -0800

    Enable submodule through the ui

--- tests/codeception/acceptance/StanfordEventsSeriesCest.php
+++ tests/codeception/acceptance/StanfordEventsSeriesCest.php
@@ -12,7 +12,18 @@ class StanfordEventsSeriesCest {
    * Events Series Module Enable.
    */
   public function _before(AcceptanceTester $I) {
-    $I->runDrush('pm:enable stanford_events_series -y');
+    // Log in and enable the module through the UI.
+    $I->logInWithRole('administrator');
+    $I->amOnPage('/admin/modules');
+    $enabled = $I->grabAttributeFrom('input[name="modules[stanford_events_series][enable]"]', 'checked');
+    if (!$enabled) {
+      $I->checkOption('Stanford Events Series');
+      $I->click('Install');
+      $I->click('Continue');
+    }
+    $I->amOnPage('/admin/structure/types/manage/stanford_event_series/fields');
+    $I->canSeeResponseCodeIs(200);
+    $I->amOnPage('/user/logout');
   }
 
   /**
@@ -60,7 +71,7 @@ class StanfordEventsSeriesCest {
    */
   protected function createEventSeriesNode(AcceptanceTester $I, $node_title = NULL) {
     $event_nodes = [];
-    for($i = 0; $i <= 5; $i++) {
+    for ($i = 0; $i <= 5; $i++) {
       $node = $this->createEventNode($I, "Series Event Node: $i", $i);
       $event_nodes[] = ['target_id' => $node->id()];
     }
@@ -77,56 +88,56 @@ class StanfordEventsSeriesCest {
     return $e;
   }
 
-   /**
-    * Create event nodes.
-    */
-   protected function createEventNode(AcceptanceTester $I, $node_title = null, $time_multiplier = 1) {
-     $start = time() - (60 * 60 * 24 * $time_multiplier);
-     $end = time() + (60 * 60 * 24 * $time_multiplier);
+  /**
+   * Create event nodes.
+   */
+  protected function createEventNode(AcceptanceTester $I, $node_title = NULL, $time_multiplier = 1) {
+    $start = time() - (60 * 60 * 24 * $time_multiplier);
+    $end = time() + (60 * 60 * 24 * $time_multiplier);
 
-     $e = $I->createEntity([
-       'type' => 'stanford_event',
-       'title' => $node_title ?: 'This is a test event node',
-       'body' => [
-         "value" => "<p>More updates to come.</p>",
-         "summary" => "",
-       ],
-       'su_event_date_time' => [
-         'value' => $start,
-         'end_value' => $end,
-         'duration' => floor(($start - $end) / 60),
-         'timezone' => "America/Los_Angeles",
-       ],
-       'su_event_dek' => 'This is a dek field',
-       'su_event_sponsor' => [
-         'This is a sponsor',
-       ],
-       'su_event_subheadline' => 'This is a sub-headline',
-     ]);
-     $I->runDrush('cache:rebuild');
-     return $e;
-   }
+    $e = $I->createEntity([
+      'type' => 'stanford_event',
+      'title' => $node_title ?: 'This is a test event node',
+      'body' => [
+        "value" => "<p>More updates to come.</p>",
+        "summary" => "",
+      ],
+      'su_event_date_time' => [
+        'value' => $start,
+        'end_value' => $end,
+        'duration' => floor(($start - $end) / 60),
+        'timezone' => "America/Los_Angeles",
+      ],
+      'su_event_dek' => 'This is a dek field',
+      'su_event_sponsor' => [
+        'This is a sponsor',
+      ],
+      'su_event_subheadline' => 'This is a sub-headline',
+    ]);
+    $I->runDrush('cache:rebuild');
+    return $e;
+  }
 
-   /**
-    * Test for Views.
-    */
-   public function testForViewsExist(AcceptanceTester $I) {
-     $I->logInWithRole('administrator');
-     $I->amOnPage("/admin/structure/views");
-     $I->canSee("stanford_event_series");
-   }
+  /**
+   * Test for Views.
+   */
+  public function testForViewsExist(AcceptanceTester $I) {
+    $I->logInWithRole('administrator');
+    $I->amOnPage("/admin/structure/views");
+    $I->canSee("stanford_event_series");
+  }
 
-   /**
-    * Test for Page Manager Pages.
-    */
-   public function testForPageManagerPagesExist(AcceptanceTester $I) {
-     $I->logInWithRole('administrator');
-     // Admin.
-     $I->amOnPage("/admin/structure/page_manager");
-     $I->canSee("event_series");
-     // Front End.
-     $I->amOnPage("/event-series");
-     $I->canSeeResponseCodeIs(200);
-   }
+  /**
+   * Test for Page Manager Pages.
+   */
+  public function testForPageManagerPagesExist(AcceptanceTester $I) {
+    $I->logInWithRole('administrator');
+    // Admin.
+    $I->amOnPage("/admin/structure/page_manager");
+    $I->canSee("event_series");
+    // Front End.
+    $I->amOnPage("/event-series");
+    $I->canSeeResponseCodeIs(200);
+  }
 
 }
